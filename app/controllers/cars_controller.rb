3bf71048f1b1790dac196c2aaf8e6a5bb79cb218
Damien Baubriau <dam.baubriau@gmail.com>
2024-10-03 16:27:09 +0100

    Add index and show in Cars Controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,11 @@
 class CarsController < ApplicationController
   def index
+    @cars = Car.all
   end
 
   def show
+    @car = Car.find(params[:id])
+    @reviews = @car.reviews
+    @favourite = @car.favourites.find_by(user: current_user)
   end
 end
