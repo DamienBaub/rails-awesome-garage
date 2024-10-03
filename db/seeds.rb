f5d6bc64b2997b3085c41bfdaad7228bfdb0ca2a
Damien Baubriau <dam.baubriau@gmail.com>
2024-10-03 14:37:45 +0100

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-Car.destory_all
+puts "Cleaning DB..."
 
-Car.create(brand: "Tesla", model: "Model X Plaid", year: 2020, fuel:"Electric")
-Car.create(brand: "Tesla", model: "Cybertruck", year: 2025, fuel:"Electric")
+Review.destroy_all
+Favourite.destroy_all
+Car.destroy_all
+Owner.destroy_all
+
+puts "Creating owners..."
+
+owner1 = Owner.create!(nickname: "Tera")
+owner2 = Owner.create!(nickname: "Dam")
+
+puts "Creating cars..."
+
+Car.create!(brand: "Tesla", model: "Model X Plaid", year: 2020, fuel:"Electric", owner: owner1)
+Car.create!(brand: "Tesla", model: "Cybertruck", year: 2025, fuel:"Electric", owner: owner2)
