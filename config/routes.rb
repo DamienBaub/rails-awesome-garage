36d8a04a861bdfe30532fa5923283b77d97d21df
Damien Baubriau <dam.baubriau@gmail.com>
2024-10-04 09:10:32 +0100

    Add favourites and reviews controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "cars/index"
-  get "cars/show"
+  get "reviews/create"
+  get "favourites/index"
+  get "favourites/create"
+  get "favourites/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +15,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :cars, only: [ :index, :show ]
+  resources :cars, only: [ :index, :show ] do
+    resources :reviews, only: :create
+    resources :favourites, only: :create
+  end
+
+  resources :favourites, only: [ :index, :destroy ]
+  # get '/favourites', to: 'favourites#index'
 end
